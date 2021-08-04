4c01eceea7dad08d3f2e2ed58c283309996b765a
gmy-micky <mustafayousaf9876@gmail.com>
2021-08-04 11:52:05 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ function App() {
   const [inter,setInter]= useState()
   const [controlTime,setControlTime]=useState(true)
   const [before,setBefore]=useState('')
-  const [after,setAfter]=useState('')
+  //const [after,setAfter]=useState('')
   const [dataTable,setDataTable]=useState([])   
 
   let updateH=time.h,updateM=time.m,updateS=time.s;
+   
 
   const timeChange = () =>{
     setControlTime(!controlTime)
-    setBefore( time.h+':'+ time.m+':'+ time.s)
+    const d= new Date()
+    setBefore((time.h+d.getHours())+':'+ (time.m+d.getMinutes())+':'+ (time.s+d.getSeconds()))
     setInter(setInterval(timeRun,1000))
   }
 
@@ -38,11 +40,12 @@ function App() {
   
   const stopTime = () =>{
     clearInterval(inter) 
-    setAfter(time.h+':'+ time.m+':'+ time.s)
     addToTable()
   }
 
   const addToTable=() =>{
+    const d= new Date()
+    const after=(time.h+d.getHours())+':'+ (time.m+d.getMinutes())+':'+ (time.s+d.getSeconds());
     setDataTable([...dataTable, {before ,after}])
     setControlTime(!controlTime)
     console.log(after)
--- src/components/AddData.js
+++ src/components/AddData.js
@@ -5,14 +5,20 @@ const AddData = ({dataTable}) => {
       const fullDate = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear()   
      
     return (
-        <div className='data-container'>
+        <div className='table'>
+        <div className='head data-container'>
+              <div>DATE</div>
+              <div>CLOCK IN</div>
+              <div>CLOCK OUT</div>
+              </div>
             {dataTable.map((data)=>(
-                <>
+                <div className='body data-container'>
                   <div>{fullDate}</div>
                   <div>{data.before}</div>
                   <div>{data.after}</div> 
-                </>
+                </div>
             ))}
+            
         </div>
     )
 }
